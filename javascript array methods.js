b171e1821642644192d538251f31fca21aef8e4a
Uttam <uttamrohit4545@gmail.com>
2025-05-15 23:05:57 +0600

    filter update

--- javascript array methods.js
+++ javascript array methods.js
@@ -7,33 +7,50 @@
 // console.log(newArr);
 
 
-// let users = [
-//     {
-//         name: "Uttam Sharma",
-//         age: 25,
-//         interests: ["JavaScript", "Math"]
-//     },
-//     {
-//         name: "Bormon",
-//         age: 23,
-//         interests: ["Movie", "Reading"]
-//     },
-//     {
-//         name: "Sadia",
-//         age: 22,
-//         interests: ["Coding", "Design"]
-//     }
-// ];
+
+let users = [
+    {
+        name: "Uttam Sharma",
+        age: 25,
+        interests: ["JavaScript", "Math"]
+    },
+    {
+        name: "Bormon",
+        age: 18,
+        interests: ["Movie", "Reading"]
+    },
+    {
+        name: "Sadia",
+        age: 22,
+        interests: ["Coding", "Design"]
+    }
+];
 
 
-// let newArr = users.map((el) => {
-//     return {
-//         name: el.name,
-//         age: el.age,
-//     };
-// });
+let newArr = users.map((el) => {
+    return {
+        name: el.name,
+        age: el.age,
+    };
+});
 
+console.log(newArr);
+
+
+let agegreaterthan20 = users.filter((el)=> {
+     return el.age > 20;
+});
+console.log(agegreaterthan20);
+
+
+
+// Filters - ekhane ager array theke filter kore notun array create hobe
+
+// let nums = [2, 3, 1, 8, 9];
+// let newArr = nums.filter((el) => {
+//     if (el > 5) {
+//         return el;
+//     }   
+//  });
 // console.log(newArr);
-
-
 
